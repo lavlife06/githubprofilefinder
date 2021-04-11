b1071f5cfa09c706a9232b6b9e5298c80b2493b6
lavlife06 <lav7990192369@gmail.com>
2021-04-11 11:11:21 +0530

    not found component changed

--- src/components/usersdata/Notfound.js
+++ src/components/usersdata/Notfound.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Notfound = () => {
   return (
     <div>
       <h1>Not Found</h1>
-      <p>The Movie u r looking for is being not found!!</p>
+      <p>The page u r looking for is being not found!!</p>
     </div>
-  )
-}
+  );
+};
 
-export default Notfound
+export default Notfound;
