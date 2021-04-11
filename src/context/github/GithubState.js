92dbd8c78a2e6e54e70819780130d3de856b369a
lavlife06 <lav7990192369@gmail.com>
2021-04-11 10:44:55 +0530

    many changs and fixed some bugs

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,12 +1,12 @@
-import React, { useReducer } from 'react';
-import GithubReducer from './githubReducer';
-import GithubContext from './githubContext';
+import React, { useReducer } from "react";
+import GithubReducer from "./githubReducer";
+import GithubContext from "./githubContext";
 import {
   SEARCH_USERS,
   CLEAR_USERS,
   GET_USERS_REPOS,
-  GET_USERS
-} from '../types';
+  GET_USERS,
+} from "../types";
 
 //useReducer is an alternative to useState.
 
@@ -17,81 +17,91 @@ const GithubState = (props) => {
     createdRepos: [],
     readme: [],
     user_repo_url: [],
-    repos_id: []
-  }
+    repos_id: [],
+  };
   const [state, dispatch] = useReducer(GithubReducer, initialState);
-
-  const github_id = "d20b59a667ed4b931be6";
-  const github_secret = "4a293f41b9f53bbbec836a53fe3d0c88f78d97c6"
 
   // Search Users
   const searchUsers = async (text) => {
-    
-    let response = await fetch(`https://api.github.com/search/users?q=${text}&client_id=${github_id}&client_secret=${github_secret}`);
+    let response = await fetch(`https://api.github.com/search/users?q=${text}`);
     let data = await response.json();
-    let items = await data.items
+    let items = await data.items;
 
     dispatch({
       type: SEARCH_USERS,
-      payload: items // It is the actual data which we want to set or send 
+      payload: items, // It is the actual data which we want to set or send
     });
   };
 
   // Get User
-    const getUser = async (username) => {
-    let response = await fetch(`https://api.github.com/users/${username}?client_id=${github_id}&client_secret=${github_secret}`);
+  const getUser = async (username) => {
+    dispatch({
+      type: CLEAR_USERS,
+    });
+    console.log(state.createdRepos, "repos");
+    let response = await fetch(`https://api.github.com/users/${username}`);
     let particulardata = await response.json();
-  
-    dispatch({type: GET_USERS,payload: particulardata});
+
+    dispatch({ type: GET_USERS, payload: particulardata });
   };
- 
+
   // Get User Repos
   const getUserRepos = async (username) => {
-    let response = await fetch(`https://api.github.com/users/${username}/repos?client_id=${github_id}&client_secret=${github_secret}`);
+    dispatch({
+      type: CLEAR_USERS,
+    });
+    let response = await fetch(
+      `https://api.github.com/users/${username}/repos`
+    );
     let userRepos = await response.json();
     let top5 = 0;
-    userRepos.forEach(element => {
-      if(element.fork === false && top5 <=4 ){
-      state.createdRepos.push(element.name);
-      state.readme.push(element.description);
-      state.user_repo_url.push(element.html_url);
-      state.repos_id.push(element.id)
-      top5 +=1
-    }
+    userRepos.forEach((element) => {
+      if (element.fork === false && top5 <= 4) {
+        state.createdRepos.push(element.name);
+        state.readme.push(element.description);
+        state.user_repo_url.push(element.html_url);
+        state.repos_id.push(element.id);
+        top5 += 1;
+      }
     });
-    console.log(state.readme)
+    console.log(state.readme);
     // setCreatedRepos(createdRepos);
     // setReadme(readme);
     // setUser_repo_url(user_repo_url);
     dispatch({
       type: GET_USERS_REPOS,
-      payload: { createdRepos: state.createdRepos, readme: state.readme, user_repo_url: state.user_repo_url, repos_id: state.repos_id }
-    })
+      payload: {
+        createdRepos: state.createdRepos,
+        readme: state.readme,
+        user_repo_url: state.user_repo_url,
+        repos_id: state.repos_id,
+      },
+    });
   };
   // Clear Users
-  const clearUsers = () => dispatch({type: CLEAR_USERS});
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
-  return (<GithubContext.Provider
-    value={{
-      users: state.users,
-      particularuser: state.particularuser,
-      createdRepos: state.createdRepos,
-      user_repo_url: state.user_repo_url,
-      readme: state.readme,
-      repos_id: state.repos_id,
-      searchUsers,
-      clearUsers,
-      getUser,
-      getUserRepos
-    }}
-  >
-    {props.children}
-  </GithubContext.Provider>)
-}
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        particularuser: state.particularuser,
+        createdRepos: state.createdRepos,
+        user_repo_url: state.user_repo_url,
+        readme: state.readme,
+        repos_id: state.repos_id,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
+  );
+};
 
 // https://api.github.com/users/lavlife06/repos
-  // https://api.github.com/repos/lavlife06/mycalculator/contents/README.md?ref=master
-
+// https://api.github.com/repos/lavlife06/mycalculator/contents/README.md?ref=master
 
 export default GithubState;
-
--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -2,40 +2,49 @@ import {
   SEARCH_USERS,
   CLEAR_USERS,
   GET_USERS_REPOS,
-  GET_USERS
-} from '../types';
+  GET_USERS,
+} from "../types";
 
 const GithubReducer = (state, action) => {
-  switch(action.type){
+  switch (action.type) {
     case SEARCH_USERS:
       return {
         ...state,
         users: action.payload,
+        particularuser: {},
+        createdRepos: [],
+        readme: [],
+        user_repo_url: [],
+        repos_id: [],
       };
     case CLEAR_USERS:
       return {
-        ...state,
-        users: []
+        users: [],
+        particularuser: {},
+        createdRepos: [],
+        readme: [],
+        user_repo_url: [],
+        repos_id: [],
       };
     case GET_USERS:
       return {
         ...state,
-        particularuser: action.payload
+        particularuser: action.payload,
       };
     case GET_USERS_REPOS:
-      return{
+      return {
         ...state,
         createdRepos: action.payload.createdRepos,
         readme: action.payload.readme,
         user_repo_url: action.payload.user_repo_url,
-        repos_id: action.payload.repos_id
-      }
+        repos_id: action.payload.repos_id,
+      };
     default:
       return state;
   }
-}
+};
 
 export default GithubReducer;
-    // setCreatedRepos(createdRepos);
-    // setReadme(readme);
-    // setUser_repo_url(user_repo_url);
+// setCreatedRepos(createdRepos);
+// setReadme(readme);
+// setUser_repo_url(user_repo_url);
