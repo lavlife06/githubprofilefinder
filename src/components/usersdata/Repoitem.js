734f20576b3fb1cbf96b791b88cc5a1ef5e09142
lavlife06 <lav7990192369@gmail.com>
2021-04-11 11:01:18 +0530

    routing changes

--- src/components/usersdata/Repoitem.js
+++ src/components/usersdata/Repoitem.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const RepoItem = ({ repo, top5readme, repo_url }) => {
   return (
-    <div className='card'>
+    <div className="card">
       <h3>
-        <a href={repo_url}>{repo}</a>
+        <a href={repo_url} target="_blank">
+          {repo}
+        </a>
         <p>{top5readme}</p>
       </h3>
     </div>
