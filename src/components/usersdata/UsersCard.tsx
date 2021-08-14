cf21b39eaf7c9c6f5009ee7b2376da2f84f2548d
lavlife06 <lav7990192369@gmail.com>
2021-08-14 18:48:03 +0530

    renamed every .js file to .tsx file

---
+++ src/components/usersdata/UsersCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface AppProps {
+    user: { login: string; avatar_url: string };
+}
+
+const UsersCard = ({ user: { login, avatar_url } }: AppProps) => {
+    return (
+        <div className="card text-centre">
+            <img
+                src={avatar_url}
+                alt=""
+                className="round-img"
+                style={{ width: "60px" }}
+            />
+            <h3>{login}</h3>
+            <Link
+                to={`/githubers/${login}`}
+                className="btn btn-dark btn-sm my-1"
+            >
+                More
+            </Link>
+        </div>
+    );
+};
+
+export default UsersCard;
