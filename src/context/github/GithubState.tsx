dd9356862baceb6523d59fb36e95d575849ba521
lavlife06 <lav7990192369@gmail.com>
2021-08-19 21:30:23 +0530

    added GithubState to typescript file

--- src/context/github/GithubState.tsx
+++ src/context/github/GithubState.tsx
@@ -1,104 +1,124 @@
 import React, { useReducer } from "react";
 import GithubReducer from "./githubReducer";
-import GithubContext from "./githubContext";
-import {
-  SEARCH_USERS,
-  CLEAR_USERS,
-  GET_USERS_REPOS,
-  GET_USERS,
-} from "../types";
+import { GithubContext } from "./githubContext";
+import { ActionKind } from "../types";
+import { InitialState } from "../../interfaces/contextInterfaces";
 
 //useReducer is an alternative to useState.
 
-const GithubState = (props) => {
-  const initialState = {
-    users: [],
-    particularuser: {},
-    createdRepos: [],
-    readme: [],
-    user_repo_url: [],
-    repos_id: [],
-  };
-  const [state, dispatch] = useReducer(GithubReducer, initialState);
+interface InputProps {
+    children: React.ReactNode;
+}
 
-  // Search Users
-  const searchUsers = async (text) => {
-    let response = await fetch(`https://api.github.com/search/users?q=${text}`);
-    let data = await response.json();
-    let items = await data.items;
+const GithubState = ({ children }: InputProps) => {
+    const initialState: InitialState = {
+        users: [],
+        particularuser: {},
+        createdRepos: [],
+        readme: [],
+        user_repo_url: [],
+        repos_id: [],
+    };
 
-    dispatch({
-      type: SEARCH_USERS,
-      payload: items, // It is the actual data which we want to set or send
-    });
-  };
+    const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-  // Get User
-  const getUser = async (username) => {
-    dispatch({
-      type: CLEAR_USERS,
-    });
-    console.log(state.createdRepos, "repos");
-    let response = await fetch(`https://api.github.com/users/${username}`);
-    let particulardata = await response.json();
+    // Search Users
+    const searchUsers = async (text: string): Promise<void> => {
+        let response = await fetch(
+            `https://api.github.com/search/users?q=${text}`
+        );
+        let data = await response.json();
+        let items = await data.items;
 
-    dispatch({ type: GET_USERS, payload: particulardata });
-  };
+        let newList = items.map((user: typeof items[0]) => {
+            return { login: user.login, avatar_url: user.avatar_url };
+        });
 
-  // Get User Repos
-  const getUserRepos = async (username) => {
-    dispatch({
-      type: CLEAR_USERS,
-    });
-    let response = await fetch(
-      `https://api.github.com/users/${username}/repos`
+        dispatch({
+            type: ActionKind.SEARCH_USERS,
+            payload: newList, // It is the actual data which we want to set or send
+        });
+    };
+
+    // Get User
+    const getUser = async (username: string): Promise<void> => {
+        dispatch({
+            type: ActionKind.CLEAR_USERS,
+        });
+        console.log(state.createdRepos, "repos");
+        let response = await fetch(`https://api.github.com/users/${username}`);
+        let particulardata = await response.json();
+
+        let newObj = {
+            name: particulardata.name,
+            avatar_url: particulardata.avatar_url,
+            bio: particulardata.bio,
+            login: particulardata.login,
+            html_url: particulardata.html_url,
+            followers: particulardata.followers,
+            following: particulardata.following,
+            public_repos: particulardata.public_repos,
+            blog: particulardata.blog,
+            public_gists: particulardata.public_gists,
+            location: particulardata.location,
+            company: particulardata.company,
+        };
+
+        dispatch({ type: ActionKind.GET_USERS, payload: newObj });
+    };
+
+    // Get User Repos
+    const getUserRepos = async (username: string): Promise<void> => {
+        dispatch({
+            type: ActionKind.CLEAR_USERS,
+        });
+        let response = await fetch(
+            `https://api.github.com/users/${username}/repos`
+        );
+        let userRepos = await response.json();
+        let top5 = 0;
+        userRepos.forEach((element: typeof userRepos[0]) => {
+            if (element.fork === false && top5 <= 4) {
+                state.createdRepos.push(element.name);
+                state.readme.push(element.description);
+                state.user_repo_url.push(element.html_url);
+                state.repos_id.push(element.id);
+                top5 += 1;
+            }
+        });
+        console.log(state.readme);
+
+        dispatch({
+            type: ActionKind.GET_USERS_REPOS,
+            payload: {
+                createdRepos: state.createdRepos,
+                readme: state.readme,
+                user_repo_url: state.user_repo_url,
+                repos_id: state.repos_id,
+            },
+        });
+    };
+    // Clear Users
+    const clearUsers = (): void => dispatch({ type: ActionKind.CLEAR_USERS });
+
+    return (
+        <GithubContext.Provider
+            value={{
+                users: state.users,
+                particularuser: state.particularuser,
+                createdRepos: state.createdRepos,
+                user_repo_url: state.user_repo_url,
+                readme: state.readme,
+                repos_id: state.repos_id,
+                searchUsers,
+                clearUsers,
+                getUser,
+                getUserRepos,
+            }}
+        >
+            {children}
+        </GithubContext.Provider>
     );
-    let userRepos = await response.json();
-    let top5 = 0;
-    userRepos.forEach((element) => {
-      if (element.fork === false && top5 <= 4) {
-        state.createdRepos.push(element.name);
-        state.readme.push(element.description);
-        state.user_repo_url.push(element.html_url);
-        state.repos_id.push(element.id);
-        top5 += 1;
-      }
-    });
-    console.log(state.readme);
-    // setCreatedRepos(createdRepos);
-    // setReadme(readme);
-    // setUser_repo_url(user_repo_url);
-    dispatch({
-      type: GET_USERS_REPOS,
-      payload: {
-        createdRepos: state.createdRepos,
-        readme: state.readme,
-        user_repo_url: state.user_repo_url,
-        repos_id: state.repos_id,
-      },
-    });
-  };
-  // Clear Users
-  const clearUsers = () => dispatch({ type: CLEAR_USERS });
-
-  return (
-    <GithubContext.Provider
-      value={{
-        users: state.users,
-        particularuser: state.particularuser,
-        createdRepos: state.createdRepos,
-        user_repo_url: state.user_repo_url,
-        readme: state.readme,
-        repos_id: state.repos_id,
-        searchUsers,
-        clearUsers,
-        getUser,
-        getUserRepos,
-      }}
-    >
-      {props.children}
-    </GithubContext.Provider>
-  );
 };
 
 // https://api.github.com/users/lavlife06/repos
