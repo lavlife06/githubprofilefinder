ab1d90c065008d595dfe09cf3af5517e128f2e29
lavlife06 <lav7990192369@gmail.com>
2021-08-19 21:29:31 +0530

    added githubReducer to typescript file

--- src/context/github/githubReducer.tsx
+++ src/context/github/githubReducer.tsx
@@ -1,50 +1,44 @@
-import {
-  SEARCH_USERS,
-  CLEAR_USERS,
-  GET_USERS_REPOS,
-  GET_USERS,
-} from "../types";
+import { ActionKind } from "../types";
 
-const GithubReducer = (state, action) => {
-  switch (action.type) {
-    case SEARCH_USERS:
-      return {
-        ...state,
-        users: action.payload,
-        particularuser: {},
-        createdRepos: [],
-        readme: [],
-        user_repo_url: [],
-        repos_id: [],
-      };
-    case CLEAR_USERS:
-      return {
-        users: [],
-        particularuser: {},
-        createdRepos: [],
-        readme: [],
-        user_repo_url: [],
-        repos_id: [],
-      };
-    case GET_USERS:
-      return {
-        ...state,
-        particularuser: action.payload,
-      };
-    case GET_USERS_REPOS:
-      return {
-        ...state,
-        createdRepos: action.payload.createdRepos,
-        readme: action.payload.readme,
-        user_repo_url: action.payload.user_repo_url,
-        repos_id: action.payload.repos_id,
-      };
-    default:
-      return state;
-  }
+import { InitialState, Action } from "../../interfaces/contextInterfaces";
+
+const GithubReducer = (state: InitialState, action: Action): InitialState => {
+    switch (action.type) {
+        case ActionKind.SEARCH_USERS:
+            return {
+                ...state,
+                users: action.payload,
+                particularuser: {},
+                createdRepos: [],
+                readme: [],
+                user_repo_url: [],
+                repos_id: [],
+            };
+        case ActionKind.CLEAR_USERS:
+            return {
+                users: [],
+                particularuser: {},
+                createdRepos: [],
+                readme: [],
+                user_repo_url: [],
+                repos_id: [],
+            };
+        case ActionKind.GET_USERS:
+            return {
+                ...state,
+                particularuser: action.payload,
+            };
+        case ActionKind.GET_USERS_REPOS:
+            return {
+                ...state,
+                createdRepos: action.payload.createdRepos,
+                readme: action.payload.readme,
+                user_repo_url: action.payload.user_repo_url,
+                repos_id: action.payload.repos_id,
+            };
+        default:
+            return state;
+    }
 };
 
 export default GithubReducer;
-// setCreatedRepos(createdRepos);
-// setReadme(readme);
-// setUser_repo_url(user_repo_url);
