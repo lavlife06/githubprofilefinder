82d7c82cd9d3e4547e9921820b11013ab6f264ef
lavlife06 <lav7990192369@gmail.com>
2021-08-19 21:33:43 +0530

    changed export types to enum

--- src/context/types.tsx
+++ src/context/types.tsx
@@ -1,5 +1,6 @@
-export const SEARCH_USERS = 'SEARCH_USERS';
-export const CLEAR_USERS = 'CLEAR_USERS';
-export const GET_USERS_REPOS = 'GET_USERS_REPOS';
-export const GET_USERS = 'GET_USERS';
-
+export const enum ActionKind {
+    SEARCH_USERS = "SEARCH_USERS",
+    CLEAR_USERS = "CLEAR_USERS",
+    GET_USERS_REPOS = "GET_USERS_REPOS",
+    GET_USERS = "GET_USERS",
+}
