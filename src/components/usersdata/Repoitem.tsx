bd8c0c701e55de9727af556b5ee172748a38f494
lavlife06 <lav7990192369@gmail.com>
2021-09-11 21:48:31 +0530

    changed styling of repo container

--- src/components/usersdata/Repoitem.tsx
+++ src/components/usersdata/Repoitem.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useGlobalContext } from "../../context/github/githubContext";
+import "./usersdata.css";
 
 interface AppProps {
     repo: string;
@@ -7,14 +9,39 @@ interface AppProps {
 }
 
 const RepoItem = ({ repo, top5readme, repo_url }: AppProps) => {
+    const { darkMode } = useGlobalContext();
+
     return (
-        <div className="card">
-            <h3>
+        <div className="reposContainer" style={{ borderStyle: "none" }}>
+            {/* <h3>
                 <a href={repo_url} rel="noopener noreferrer" target="_blank">
                     {repo}
                 </a>
                 <p>{top5readme}</p>
-            </h3>
+            </h3> */}
+            <div
+                className={
+                    darkMode ? "dark-repo-details" : "white-repo-details"
+                }
+            >
+                <h3 style={{ textDecoration: "underline" }}>
+                    <a
+                        href={repo_url}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        style={{ color: "black" }}
+                    >
+                        {repo}
+                    </a>
+                </h3>
+                <div className="fork-star">
+                    <p>Forks: {2}</p>
+                    <p id="star">Stars: {3}</p>
+                </div>
+                <p id="description">
+                    <em>{top5readme ? top5readme : "No Description"}</em>
+                </p>
+            </div>
         </div>
     );
 };
