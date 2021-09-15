91599e8d471196446ed22b58fc2a2387dbfa07e8
lavlife06 <lav7990192369@gmail.com>
2021-09-15 11:28:26 +0530

    add fileds like followers, following and public_repos in UsersCard

--- src/components/usersdata/UsersCard.tsx
+++ src/components/usersdata/UsersCard.tsx
@@ -1,26 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./usersdata.css";
 
 interface AppProps {
     user: { login: string; avatar_url: string };
+    mode: boolean;
 }
 
-const UsersCard = ({ user: { login, avatar_url } }: AppProps) => {
+const UsersCard = ({ user: { login, avatar_url }, mode }: AppProps) => {
     return (
-        <div className="card text-centre">
-            <img
-                src={avatar_url}
-                alt=""
-                className="round-img"
-                style={{ width: "60px" }}
-            />
-            <h3>{login}</h3>
+        <div className={mode ? "dark-user-details" : "white-user-details"}>
+            <div className="user-photo">
+                <img src={avatar_url} alt={login} />
+                <h3>{login}</h3>
+            </div>
+            <div className="other-details">
+                <ul>
+                    {true ? (
+                        <li>
+                            Followers: <span className="data">{5}</span>
+                        </li>
+                    ) : null}
+                    {true ? (
+                        <li>
+                            Following: <span className="data">{4}</span>
+                        </li>
+                    ) : null}
+                    {true ? (
+                        <li>
+                            Repositories: <span className="data">{4}</span>
+                        </li>
+                    ) : null}
+                </ul>
+            </div>
             <Link
                 to={`/githubers/${login}`}
-                className="btn btn-dark btn-sm my-1"
+                className={`btn ${mode ? "btn-light" : "btn-dark"} btn-sm my-1`}
             >
                 More
             </Link>
+            <div></div>
         </div>
     );
 };
