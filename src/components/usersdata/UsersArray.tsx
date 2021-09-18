d8a0ad988193b82ccbb7ffa673e2a0a68678f77a
lavlife06 <lav7990192369@gmail.com>
2021-09-18 20:23:06 +0530

    added additional checking of users data presence

--- src/components/usersdata/UsersArray.tsx
+++ src/components/usersdata/UsersArray.tsx
@@ -5,11 +5,14 @@ import { useGlobalContext } from "../../context/github/githubContext";
 const UsersArray = () => {
     const { users, darkMode } = useGlobalContext();
 
+    console.log(users);
     return (
         <div className="usersArrayContainer">
-            {users.map((user, index) => (
-                <UsersCard key={index} user={user} mode={darkMode} />
-            ))}
+            {users.length
+                ? users.map((user, index) => (
+                      <UsersCard key={index} user={user} mode={darkMode} />
+                  ))
+                : null}
         </div>
     );
 };
